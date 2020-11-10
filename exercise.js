b03f776842993d0a2d5fe1aedd3e4c5f0cca9f14
Sebastian Glaser <hakt0r@users.noreply.github.com>
2020-11-10 13:40:01 +0100

    one

---
+++ exercise.js
@@ -0,0 +1,43 @@
+/* 1. Combining Arrays
+    Create two arrays. e.g. euroCountries and asianCountries.
+    Add asianCountries items to the end of euroCountries array.
+    Once again create two arrays.
+    Save all elements of both arrays to another variable.
+*/
+
+
+
+/* 2. Copying Arrays
+    Copy an array using the spread operator.
+    Store the copied array in another variable.
+*/
+
+// 2.1. Copy
+const arr  = [1,2,3];
+const copy = ???
+
+// 2.2 Create another copy, add 4 at the end of it
+
+const copyPush = ???
+
+// 2.3 Create another copy, add 4 at the end of it
+
+const copyUnshift = ???
+
+/* 3. Clone and Merge
+    Given two objects:
+*/
+const person = {name: "John"}
+const job = {role: "Teacher"}
+
+// 3.1 Clone the person object.
+
+const clone = ???
+
+// 3.2 Merge these two objects into one object: "employee". Use the spread operator to do so.
+
+const employee = ???
+
+// 3.3 Then change the values of the properties
+//     in the employee object.
+
